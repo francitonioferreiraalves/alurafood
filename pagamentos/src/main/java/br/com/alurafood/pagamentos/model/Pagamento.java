d578ecc667525db6fc6feaf2fcf23b5d0063d75e
francitonioferreiraalves <francitonio@gmail.com>
2022-10-10 18:18:25 -0300

    Criação gateway e server

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
@@ -9,7 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
@@ -30,26 +29,30 @@ public class Pagamento {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	@NotNull
 	@Positive
 	private BigDecimal valor;
-	@NotBlank
+
 	@Size(max = 100)
 	private String nome;
-	@NotBlank
+
 	@Size(max = 19)
 	private String numero;
-	@NotBlank
+
 	@Size(max = 7)
 	private String expiracao;
-	@NotBlank
+
 	@Size(min = 3, max = 3)
 	private String codigo;
+
 	@NotNull
 	@Enumerated(EnumType.STRING)
 	private Status status;
+
 	@NotNull
 	private Long pedidoId;
+
 	@NotNull
 	private Long formaDePagamentoId;
 
